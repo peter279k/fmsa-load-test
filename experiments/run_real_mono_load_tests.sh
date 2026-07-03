795745443a3f6b94862fe12b2d53afbf1c26ee6e
Peter <peter279k@gmail.com>
2026-07-03 13:34:21 +0800

    Add real mono load tests

---
+++ experiments/run_real_mono_load_tests.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo "Executing Load Test is started!"
+
+cd ~/fmsa
+if [[ ! -d ./fmsa-venv ]]; then
+    python3 -m venv fmsa-venv
+fi;
+
+./fmsa-venv/bin/pip install -r api_gateway/requirements.txt
+./fmsa-venv/bin/pip install -r api_gateway/requirements-dev.txt
+
+cd ~/fmsa-load-test/experiments
+
+if [[ ! -d ./fmsa-load-test-experiments ]]; then
+    python3 -m venv fmsa-load-test-experiments
+fi;
+
+./fmsa-load-test-experiments/bin/pip install -r ../requirements.txt
+
+
+host=$1
+path="scenarios.txt"
+
+if [[ ! -f "$path" ]]; then
+    echo "$path file is not found."
+    exit 1;
+fi;
+
+for file_name in $(cat $path)
+do
+    docker compose down
+    sleep 300
+    docker volume rm $(docker volume ls | grep fmsa | awk '{print $2}')
+
+    cd ~/fmsa
+
+    docker compose up -d 
+
+    sleep 600
+
+    cd ~/fmsa-load-test/experiments
+    for _ in $(1 5)
+    do
+        ./fmsa-load-test-experiments/bin/python pre_upload_required_references.py
+    done;
+
+    csv_result=$(echo $file_name | awk '{split($1,a,"."); print a[1]}')
+    ./fmsa-load-test-experiments/bin/locust -f $file_name --headless \
+        --host "http://$host" \
+        --users 1000 \
+        --spawn-rate 1.11 \
+        --run-time 20m \
+        --csv="real_mono_$csv_result"
+done;
+
+echo "Executing Load Test is done!"
