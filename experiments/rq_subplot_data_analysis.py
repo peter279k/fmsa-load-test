9a017ecd2f82a458d7a55c15202bc6908c3bdde5
peter279k <peter279k@gmail.com>
2026-07-05 22:05:15 +0800

    Fix subplots title scenario number

--- experiments/rq_subplot_data_analysis.py
+++ experiments/rq_subplot_data_analysis.py
@@ -172,7 +172,7 @@ for scenario,csv_files in mono_csv_files.items():
         )
 
         fig.supxlabel(xlabel, fontsize=11)
-        fig.suptitle(f'S{scenario[1:]}', fontsize=15)
+        fig.suptitle(f'S{scenario[1:-1]} {scenario[-1]}', fontsize=15)
 
         fig.savefig(f'{plot_dir}/fig_rq3_{scenario}_result.svg', dpi=dpi)
         fig.savefig(f'{plot_dir}/fig_rq3_{scenario}_result.png', dpi=dpi)
